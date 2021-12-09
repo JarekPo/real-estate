4a63d4cb3607f5a3750eee07659dfd30d0bc94e2
it20217 <igor.tokarev@outlook.com>
2021-12-09 22:04:45 +0100

    search function added with mock data

---
+++ scripts/mock.js
@@ -0,0 +1,59 @@
+function mockedProperties() {
+  return [{
+    location: "Dublin",
+    name: "Lucky apartment",
+    type: "Apartment",
+    bedroomCount: "3 bedroom",
+    price: 1000,
+    image: "./images/card1.jpeg"
+  },{
+    location: "Dublin",
+    name: "Sweet home apartment",
+    type: "House",
+    bedroomCount: "4+ bedroom",
+    price: 2500,
+    image: "./images/card2.jpeg"
+  },{
+    location: "Dublin",
+    name: "Dublin studio",
+    type: "Studio apartment",
+    bedroomCount: "2 bedroom",
+    price: 500,
+    image: "./images/card3.jpeg"
+  },{
+    location: "Limerick",
+    name: "Limerick apartment",
+    type: "Apartment",
+    bedroomCount: "2 bedroom",
+    price: 800,
+    image: "./images/card4.jpeg"
+  },{
+    location: "Shannon",
+    name: "Shannon lucky apartment",
+    type: "Apartment",
+    bedroomCount: "3 bedroom",
+    price: 900,
+    image: "./images/card1.jpeg"
+  },{
+    location: "Shannon",
+    name: "Sweet home apartment",
+    type: "Apartment",
+    bedroomCount: "3 bedroom",
+    price: 750,
+    image: "./images/card2.jpeg"
+  },{
+    location: "Shannon",
+    name: "Home apartment",
+    type: "Apartment",
+    bedroomCount: "3 bedroom",
+    price: 690,
+    image: "./images/card3.jpeg"
+  },{
+    location: "Shannon",
+    name: "Apartment",
+    type: "Apartment",
+    bedroomCount: "3 bedroom",
+    price: 780,
+    image: "./images/card4.jpeg"
+  }]
+}
